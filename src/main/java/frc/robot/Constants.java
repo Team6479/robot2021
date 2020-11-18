910ee812a56b4734283dd4aa96abea7d121b5308
Leo Wilson <leo@leomwilson.com>
2020-11-17 17:32:55 -0700

    Add Drivetrain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,9 +10,9 @@ package frc.robot;
  */
 public final class Constants {
   public final class DriveTrainConstants {
-    public static final int LEFT_FRONT_MOTOR_PORT = 0;
-    public static final int LEFT_BACK_MOTOR_PORT = 1;
-    public static final int RIGHT_FRONT_MOTOR_PORT = 2;
-    public static final int RIGHT_BACK_MOTOR_PORT = 3;
+    public static final int LEFT_FRONT_MOTOR_PORT = 2;
+    public static final int LEFT_BACK_MOTOR_PORT = 3;
+    public static final int RIGHT_FRONT_MOTOR_PORT = 0;
+    public static final int RIGHT_BACK_MOTOR_PORT = 1;
   }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,39 +4,18 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Spark;
 import com.team6479.lib.subsystems.TankDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveTrainConstants;
 
 public class Drivetrain extends SubsystemBase implements TankDrive {
-  private final int CPR = 4096;
-  private final double WHEEL_CIRCUMFERENCE = Math.PI * 5; //TODO: Insert actual wheel diameter
-  private final TalonSRX leftFrontMotor = new TalonSRX(DriveTrainConstants.LEFT_FRONT_MOTOR_PORT);
-  private final TalonSRX leftBackMotor = new TalonSRX(DriveTrainConstants.LEFT_BACK_MOTOR_PORT);
-  private final TalonSRX rightFrontMotor = new TalonSRX(DriveTrainConstants.RIGHT_FRONT_MOTOR_PORT);
-  private final TalonSRX rightBackMotor = new TalonSRX(DriveTrainConstants.RIGHT_BACK_MOTOR_PORT);
+  private final Spark leftFrontMotor = new Spark(DriveTrainConstants.LEFT_FRONT_MOTOR_PORT);
+  private final Spark leftBackMotor = new Spark(DriveTrainConstants.LEFT_BACK_MOTOR_PORT);
+  private final Spark rightFrontMotor = new Spark(DriveTrainConstants.RIGHT_FRONT_MOTOR_PORT);
+  private final Spark rightBackMotor = new Spark(DriveTrainConstants.RIGHT_BACK_MOTOR_PORT);
 
   public Drivetrain() {
-    // Config defaults
-    leftFrontMotor.configFactoryDefault();
-    leftBackMotor.configFactoryDefault();
-    rightFrontMotor.configFactoryDefault();
-    rightBackMotor.configFactoryDefault();
-
-    // Set back motors to follow front motors
-    leftBackMotor.follow(leftFrontMotor);
-    rightBackMotor.follow(rightFrontMotor);
-
-    // Set neutral mode to brake
-    leftFrontMotor.setNeutralMode(NeutralMode.Brake);
-    leftBackMotor.setNeutralMode(NeutralMode.Brake);
-    rightFrontMotor.setNeutralMode(NeutralMode.Brake);
-    rightBackMotor.setNeutralMode(NeutralMode.Brake);
-
-    // Set encoders
-    leftFrontMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
-    rightFrontMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
 
     // Invert Motors
     leftFrontMotor.setInverted(false);
@@ -47,24 +26,25 @@ public class Drivetrain extends SubsystemBase implements TankDrive {
 
   @Override
   public void stop() {
-    leftFrontMotor.set(ControlMode.PercentOutput, 0);
-    rightFrontMotor.set(ControlMode.PercentOutput, 0);
+    leftFrontMotor.set(0);
+    leftBackMotor.set(0);
+    rightFrontMotor.set(0);
+    rightBackMotor.set(0);
   }
 
   @Override
   public void tankDrive(double leftSpeed, double rightSpeed) {
-    leftFrontMotor.set(ControlMode.PercentOutput, leftSpeed);
-    rightFrontMotor.set(ControlMode.PercentOutput, rightSpeed);
+    leftFrontMotor.set(leftSpeed);
+    leftBackMotor.set(leftSpeed);
+    rightFrontMotor.set(rightSpeed);
+    rightBackMotor.set(rightSpeed);
   }
 
   @Override
   public void arcadeDrive(double forward, double rotation) {
-    leftFrontMotor.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, +rotation);
-    rightFrontMotor.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, -rotation);
-  }
-
-  public void straightDrive(double distance) {
-    leftFrontMotor.set(ControlMode.Position, CPR * distance / WHEEL_CIRCUMFERENCE);
-    rightFrontMotor.set(ControlMode.Position, CPR * distance / WHEEL_CIRCUMFERENCE);
+    leftFrontMotor.set(forward + rotation);
+    leftBackMotor.set(forward + rotation);
+    rightFrontMotor.set(forward - rotation);
+    rightBackMotor.set(forward - rotation);
   }
 }
