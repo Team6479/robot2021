9815855ac36e4ad87d55be1b403433b023fdd899
Eric Morgan <ericmorgan602@gmail.com>
2020-11-22 16:29:28 -0700

    update turret

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,11 +1,15 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.robot.Constants.TurretConstants;
 
 public class Turret {
+
+  int upperLimit;
+  int lowerLimit;
 
   private final TalonSRX motor = new TalonSRX(TurretConstants.MOTOR);
 
@@ -14,6 +18,7 @@ public class Turret {
 
     motor.configFactoryDefault();
     motor.setNeutralMode(NeutralMode.Brake);
+    motor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
 
     motor.setInverted(true);
     motor.setSensorPhase(true);
@@ -27,6 +32,14 @@ public class Turret {
   public void stop(){
     motor.set(ControlMode.PercentOutput, 0);
 
+  }
+
+  public double getUpperLimit() {
+    return upperLimit;
+  }
+
+  public double getLowerLimit() {
+    return lowerLimit;
   }
 
 }
